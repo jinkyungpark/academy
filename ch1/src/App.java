367297652544bcc99551a1b1cc37a3e0e093fefc
kyung <kyung5896@gmail.com>
2024-05-07 21:38:43 +0900

    App 생성

--- ch1/src/App.java
+++ ch1/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("안녕하세요");
+        System.out.println("Test");
+        System.out.println("java");
     }
 }
